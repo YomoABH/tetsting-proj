6f23e4fac80cbd49e9e852f7c8bf4fc52e7a80f1
YomoABH <yomoyomosai@gmail.com>
2025-03-07 10:52:47 +0300

    feature: first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="app">
+   
+  </div>
+</template>
+
+<style scoped>
+   .app{
+      width: 300px;
+      height: 300px;
+      background: #000;
+   }
+</style>
